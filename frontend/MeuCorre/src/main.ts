fb8b8fe109794a785e6bd9b7a85d5ba83cbfd6ae
Welton Castoldi <weltoncastoldi@hotmail.com>
2025-10-28 21:55:15 -0300

    criando frontend do app

--- frontend/MeuCorre/src/main.ts
+++ frontend/MeuCorre/src/main.ts
@@ -1,6 +1,8 @@
+/// <reference types="@angular/localize" />
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { App } from './app/app';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
