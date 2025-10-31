cadaff97417c1f4aed23133e6c4e7c5564e4bfa7
Welton Castoldi <weltoncastoldi@hotmail.com>
2025-10-31 19:09:08 -0300

    criando a dashboard

--- frontend/MeuCorre/src/app/app.ts
+++ frontend/MeuCorre/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbDropdownModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
