443bdc2d4ebcab619fab83329d8a50393a5aaac8
Welton Castoldi <weltoncastoldi@hotmail.com>
2025-11-05 19:20:10 -0300

    feat: fazendo categoria

--- frontend/MeuCorre/src/app/pages/categorias/categorias.ts
+++ frontend/MeuCorre/src/app/pages/categorias/categorias.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-categorias',
-  imports: [],
+  imports: [NgbNavModule],
   templateUrl: './categorias.html',
   styleUrl: './categorias.css',
 })
 export class Categorias {
-
+  active = 1;
 }
