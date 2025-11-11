13455aaadafe9cd32f446dd164709e4f804bf1f2
Welton Castoldi <weltoncastoldi@hotmail.com>
2025-11-10 21:55:08 -0300

    feat: adiciona modal para

--- frontend/MeuCorre/src/app/pages/categorias/categorias.ts
+++ frontend/MeuCorre/src/app/pages/categorias/categorias.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { Component, inject, signal, TemplateRef, WritableSignal } from '@angular/core';
+import { ModalDismissReasons, NgbModal, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { CategoriaModel } from './models/categoria.model';
 import { IconAvatar } from '../../shared/components/icon-avatar/icon-avatar';
 import { StatusBadge } from "../../shared/components/status-badge/status-badge";
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-categorias',
-  imports: [NgbNavModule, IconAvatar, StatusBadge],
+  imports: [NgbNavModule, IconAvatar, StatusBadge, ReactiveFormsModule],
   templateUrl: './categorias.html',
   styleUrl: './categorias.css',
 })
 export class Categorias {
+  private modalService = inject(NgbModal);
+	closeResult: WritableSignal<string> = signal('');
+
+  nome = new FormControl('');
+  descricao = new FormControl('');
+  cor = new FormControl('');
+  icone = new FormControl('');
+
   active = 1;
 
   categorias_receitas: CategoriaModel[] = [
@@ -66,4 +75,46 @@ export class Categorias {
       ativo: false
     },
   ];
+
+  open(content: TemplateRef<any>) {
+		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+			(result) => {
+				this.closeResult.set(`Closed with: ${result}`);
+			},
+			(reason) => {
+				this.closeResult.set(`Dismissed ${this.getDismissReason(reason)}`);
+			},
+		);
+	}
+
+  private getDismissReason(reason: any): string {
+		switch (reason) {
+			case ModalDismissReasons.ESC:
+				return 'by pressing ESC';
+			case ModalDismissReasons.BACKDROP_CLICK:
+				return 'by clicking on a backdrop';
+			default:
+				return `with: ${reason}`;
+		}
+	}
+
+  cadastrarCategoria(){
+    console.log(this.nome.value);
+    console.log(this.descricao.value);
+    console.log(this.cor.value);
+    console.log(this.icone.value);
+
+    this.categorias_receitas.push({
+      id: this.categorias_receitas.length + 1 + '',
+      nome: this.nome.value!,
+      descricao: this.descricao.value!,
+      cor: this.cor.value!,
+      icone: this.icone.value!,
+      ativo: true
+    });
+
+    console.log(this.categorias_receitas);
+    
+    this.modalService.dismissAll();
+  }
 }
