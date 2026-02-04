a764b6f4c29429a742366aebe7fa9cf999858e5d
Welton Castoldi <weltoncastoldi@hotmail.com>
2026-02-03 21:52:22 -0300

    feat: #13 - cria entidade Tag.

---
+++ backend/MeuCorre.Domain/Entities/Tag.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MeuCorre.Domain.Entities
+{
+    public class Tag : Entidade
+    {
+        public Guid UsuarioId { get; private set; }
+        public string Nome { get; private set; }
+        public string Cor { get; private set; }
+
+        public Tag(Guid usuarioId, string nome, string cor)
+        {
+            UsuarioId = usuarioId;
+            Nome = nome.ToLower();
+            Cor = cor;
+        }
+
+        private void ValidarEntidadeTag(string cor)
+        {
+            if (string.IsNullOrEmpty(cor))
+            {
+                return; //retorna caso a cor seja nula ou vazia
+            }
+
+            //#FF02AB
+            var corRegex = new Regex(@"^#?([0-9a-fA-F]{3}){1,2}$");
+
+            if (!corRegex.IsMatch(cor))
+            {
+                throw new Exception("A cor deve estar no formato hexadecimal");
+            }
+        }
+    }
+}
